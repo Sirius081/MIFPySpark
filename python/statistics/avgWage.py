f7fef424802ed7f5ad6e77999e62e676c8c6b885
sirius <969405352@qq.com>
2016-11-20 18:59:50 +0800

    avgWage

---
+++ python/statistics/avgWage.py
@@ -0,0 +1,17 @@
+from pyspark import SparkContext
+# split
+# filter
+# year,(sumWage,count)
+#
+sc = SparkContext()
+data = sc.textFile("/mif/lines50.txt")
+data = data.map(lambda line: line.encode('utf-8').split(",")) \
+    .filter(lambda line: line[3] == '310' and line[4] == '10' and line[5] != "") \
+    .map(lambda line: (int(line[2][0:4]), (float(line[5]), 1))) \
+    .reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) \
+    .collect()
+
+out = open('/home/edu/mif/python/zwj/output/data.txt', 'w')
+for (k, v) in data:
+    out.write(str(k) + ',' + str(v[0] / v[1]*12) + '\r\n')
+out.close()
--- python/statistics/yearIncome.py
+++ python/statistics/yearIncome.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext
 from numpy import array
+
 # 统计每一年总收入
 
 sc = SparkContext()
@@ -18,13 +19,13 @@ data = sc.textFile("/mif/mode_ac43_310.txt")
 # line.split
 # filter by category 划入统筹和划入个人 不为空
 # ((year,type),(group,single))
-#((year,type),(total_group,total_single))
+# ((year,type),(total_group,total_single))
 
 data = data \
     .map(lambda line: line.encode('utf-8').split(','))
 data = data.filter(lambda line: line[3] == '310' and line[10] != '' and line[11] != '') \
     .map(lambda line: ((int(line[2][0:4]), int(line[4])), array([float(line[10]), float(line[11])]))) \
-    .reduceByKey(lambda line1,line2:array([line1[0]+line2[0],line1[1]+line2[1]]))\
+    .reduceByKey(lambda line1, line2: array([line1[0] + line2[0], line1[1] + line2[1]])) \
     .sortByKey()
 
 data = data.collect()
