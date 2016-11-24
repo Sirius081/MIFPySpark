4550a6670b4ff812eea25b5c6c472edf68db9449
sirius <969405352@qq.com>
2016-11-24 16:54:58 +0800

    chinese file name  into english filename

--- Encoder.py
+++ Encoder.py
@@ -1,6 +1,6 @@
-fn="/home/edu/mif/data/sample/职工住院信息_lines50.txt"
+fn="/home/edu/mif/data/sample/职工住院明细信息.txt"
 reader=open(fn)
-writer=open("/home/edu/mif/data/sampleUtf-8/职工住院信息_lines50.txt","w")
+writer=open("/home/edu/mif/data/sampleUtf-8/worker_hospital_detail_lines50.txt","w")
 while True:
     line=reader.readline().decode('gbk').encode('utf-8')
     writer.write(line)
