294bc2286cef610a4cc3f021270a4a1435ff1e40
sirius <969405352@qq.com>
2016-11-24 15:15:32 +0800

    chronic dont encode

--- python/statistics/chronicList.py
+++ python/statistics/chronicList.py
@@ -3,7 +3,7 @@ from pyspark import SparkContext
 # chronicname,1
 sc=SparkContext()
 data=sc.textFile("/mif/data/sample/职工慢性病登记信息_lines50.txt")\
-    .map(lambda line :line.encode("utf-8").split(","))\
+    .map(lambda line :line.split(","))\
     .map(lambda line:((line[2]),1))\
     .reduceByKey(lambda a,b:a+b) \
     .collect()
