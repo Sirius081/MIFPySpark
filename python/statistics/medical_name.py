2f537da6ca59d1c01b001835a45377571a69f733
sirius <969405352@qq.com>
2016-12-22 19:33:07 +0800

    medical name filter by len(line)>2

--- python/statistics/medical_name.py
+++ python/statistics/medical_name.py
@@ -2,11 +2,13 @@ from pyspark import SparkContext
 
 sc = SparkContext()
 data = sc.textFile("/mif/data/worker_hospital_detail.txt")
+# data = sc.textFile("/mif/data/sample/worker_hospital_detail_lines50.txt")
 data = data.map(lambda line: line.split(","))
+data=data.filter(lambda line:len(line)>2)
 # medical_name,1
 data = data.map(lambda line: (line[2], 1))
 data = data.reduceByKey(lambda a, b: a + b)
-data = data.sortBy(lambda (k, v): (v, k),False)
+data = data.sortBy(lambda (k, v): (v, k),False).collect()
 
 out = open('/home/edu/mif/python/zwj/output/data.txt', 'w')
 for (k, v) in data:
