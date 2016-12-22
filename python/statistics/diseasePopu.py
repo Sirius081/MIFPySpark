c38b9a31a0d0b7f6c8dbe2cb38676eb732b458fa
名字想了三年 <969405352@qq.com>
2016-12-22 16:01:35 +0800

    en

--- python/statistics/diseasePopu.py
+++ python/statistics/diseasePopu.py
@@ -1,22 +1,26 @@
 from pyspark import SparkContext
+
 # split
 # (year,disease),(fee,group)
-sc=SparkContext()
+sc = SparkContext()
 
 import sys
-reload(sys)
-sys.setdefaultencoding( "utf-8" )
 
-data=sc.textFile("/mif/data/worker_hospital.txt")\
-    .map(lambda line :line.split(','))\
-    .filter(lambda line:line[6]!='' and line[15]!='')\
-    .map(lambda line:((int(line[20][-2:]),line[23]),(float(line[6]),float(line[15]))))\
-    .reduceByKey(lambda a,b:a+b)\
-    .map(lambda (k,v):((k[0],v[0],v[1]),k[1]))\
-    .sortByKey()\
+reload(sys)
+sys.setdefaultencoding("utf-8")
+# filter
+# disease,(fee,group)
+# sum
+data = sc.textFile("/mif/data/worker_hospital.txt") \
+    .map(lambda line: line.split(',')) \
+    .filter(lambda line: line[6] != '' and line[15] != '') \
+    .map(lambda line: (line[23], 1)) \
+    .reduceByKey(lambda a, b:a+b) \
+    .map(lambda (k,v):(v,k))\
+    .sortByKey() \
     .collect()
 
 out = open('/home/edu/mif/python/zwj/output/data.txt', 'w')
-for ((year,fee,group),disease) in data:
-    out.write("%s,%f,%f,%s\n" % (year,fee,group,disease))
+for (disease,count) in data:
+    out.write("%s,%d\n" % (count,disease))
 out.close()
