a3af92c14521ae0241750886dd7c062ce075d770
名字想了三年 <969405352@qq.com>
2017-01-08 19:13:36 +0800

    fdf

--- python/test.py
+++ python/test.py
@@ -1,8 +1,9 @@
-def jsim(a,b):
-    count=0.0
+def jsim(a, b):
+    count = 0.0
     for i in a:
         if i in b:
-            count+=1
-    return count/(len(set(a+b)))
+            count += 1
+    return count / (len(set(a + b)))
 
-print jsim([1,2,3],[2,3,4])
+
+print jsim([1, 2, 3], [2, 3, 4])
